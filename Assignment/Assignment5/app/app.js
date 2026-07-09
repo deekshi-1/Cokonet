43a184af586090d848b55bf001cd7e25c704ff00
deekshi-1 <deekshithc1@gmail.com>
2026-07-09 16:06:09 +0530

    sonarfix

--- Assignment/Assignment5/app/app.js
+++ Assignment/Assignment5/app/app.js
@@ -2,10 +2,13 @@ const express = require("express")
 
 const app = express()
 
+// Disable Express technology fingerprinting
+app.disable("x-powered-by")
+
 app.get("/", (req, res) => {
-
     res.send("CI CD Assignment")
-
 })
 
-app.listen(3000)
+app.listen(3000, () => {
+    console.log("Server running on port 3000")
+})
