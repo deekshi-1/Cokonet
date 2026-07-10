ab675af20f2f640db74cb48ab70cd889fbacd20c
deekshi-1 <deekshithc1@gmail.com>
2026-07-10 12:52:31 +0530

    assignment6

--- Assignment/Assignment5/app/app.js
+++ Assignment/Assignment5/app/app.js
@@ -2,41 +2,10 @@ const express = require("express")
 
 const app = express()
 
-const crypto = require("node:crypto")
-
-crypto.createHash("md5")
-
-const password = "123456"
-if(true){
-    if(true){
-        if(true){
-            if(true){
-                console.log("deep nesting")
-            }
-        }
-    }
-}
-function add(a,b){
-    return a+b
-}
-
-function add2(a,b){
-    return a+b
-}
-
-function add3(a,b){
-    return a+b
-}
-
-function add4(a,b){
-    return a+b
-}
-
 // Disable Express technology fingerprinting
 app.disable("x-powered-by")
 
 app.get("/", (req, res) => {
-    eval("console.log('unsafe')")
     res.send("CI CD Assignment")
 })
 
