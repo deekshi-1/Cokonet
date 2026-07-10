6826a84bf953bd061169be044f8d0ecd2fca90ef
deekshi-1 <deekshithc1@gmail.com>
2026-07-10 15:37:32 +0530

    sonar fixes

--- Assignment/Assignment6/app/app.js
+++ Assignment/Assignment6/app/app.js
@@ -2,32 +2,6 @@ const express = require("express")
 
 const app = express()
 
-const password = "123456"
-if(true){
-    if(true){
-        if(true){
-            if(true){
-                console.log("deep nesting")
-            }
-        }
-    }
-}
-function add(a,b){
-    return a+b
-}
-
-function add2(a,b){
-    return a+b
-}
-
-function add3(a,b){
-    return a+b
-}
-
-function add4(a,b){
-    return a+b
-}
-
 // Disable Express technology fingerprinting
 app.disable("x-powered-by")
 
