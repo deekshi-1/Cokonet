dc0de5b240480e4929154f332b5cfe9a1ebcb684
deekshi-1 <deekshithc1@gmail.com>
2026-07-10 12:33:16 +0530

    assignment-6

---
+++ Assignment/Assignment6/app/app.js
@@ -0,0 +1,14 @@
+const express = require("express")
+
+const app = express()
+
+// Disable Express technology fingerprinting
+app.disable("x-powered-by")
+
+app.get("/", (req, res) => {
+    res.send("CI CD Assignment")
+})
+
+app.listen(3000, () => {
+    console.log("Server running on port 3000")
+})
